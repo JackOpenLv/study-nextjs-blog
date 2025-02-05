915100992d9ce89a8175c17996c25262aed3502b
Mike <Mike>
2025-02-05 19:30:04 +0800

    fix bug fix app/page.tsx build error

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-8">
       <main className="flex flex-col gap-8 items-center row-start-2">
-        <h1 className="text-3xl">Ming's Zone</h1>
+        <h1 className="text-3xl">{"Ming's Zone"}</h1>
         <ol className="list-decimal">
           <li>Read some tech blog</li>
           <li>Find out produce of mine and get it if you need.</li>
